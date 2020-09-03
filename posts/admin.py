76ae45969766dd74e42a06b9a0937bd7d9401751
dyachkov-pavel <pahanthebest12345@mail.ru>
2020-09-04 01:31:05 +0300

    fix posts + templates

--- posts/admin.py
+++ posts/admin.py
@@ -4,11 +4,10 @@ from .models import Post, Group
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ("pk", "text", "pub_date", "author", "group")
-    search_fields = ("text",) 
-    list_filter = ("pub_date",) 
+    search_fields = ("text",)
+    list_filter = ("pub_date",)
     empty_value_display = "-пусто-"
 
-admin.site.register(Post, PostAdmin)
 
 class GroupAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "description")
@@ -16,4 +15,6 @@ class GroupAdmin(admin.ModelAdmin):
     list_filter = ("title",)
     empty_value_display = "-пусто-"
 
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Group, GroupAdmin)
--- posts/models.py
+++ posts/models.py
@@ -5,8 +5,8 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length = 200)
-    slug = models.SlugField(unique=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, max_length=20)
     description = models.TextField()
 
     def __str__(self):
@@ -17,4 +17,8 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, blank=True, null=True, related_name="posts")
+
+
+class Meta:
+    ordering = ("-pub_date",)
